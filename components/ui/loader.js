3e0f382080162e77453e66ed5114a85abb192e77
agnieve <107839794+agnieve@users.noreply.github.com>
2022-11-29 23:14:52 +0800

    fixed height of loader

--- components/ui/loader.js
+++ components/ui/loader.js
@@ -3,7 +3,7 @@
 export default function Loader(){
 
     return(
-        <div className="absolute z-50 top-0 bg-gradient-to-r from-white to-slate-50 h-full w-full flex justify-center items-center">
+        <div className="bg-gradient-to-r from-white to-slate-50 h-full flex justify-center items-center">
             <h2 className={'text-slate-700'}>Loading . . .</h2>
         </div>
     );
