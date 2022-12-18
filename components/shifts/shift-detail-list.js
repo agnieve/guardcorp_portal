eaee798c6801453d5302daa09b05098d9f33625c
agnieve <107839794+agnieve@users.noreply.github.com>
2022-12-18 21:43:47 +0800

    fix table for shift list

--- components/shifts/shift-detail-list.js
+++ components/shifts/shift-detail-list.js
@@ -37,7 +37,7 @@ export default function ShiftDetailList(props) {
     const columns =[
         {
             Header: "Date",
-            accessor: "date",
+            accessor: "start",
         },
         {
             Header: "Team Members",
