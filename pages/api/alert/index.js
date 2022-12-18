440d53cb39276da997c5acb3583a39d98c074a4f
agnieve <107839794+agnieve@users.noreply.github.com>
2022-12-18 21:20:03 +0800

    sort alert

--- pages/api/alert/index.js
+++ pages/api/alert/index.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
                     }
                 ]
             },
-
+            $sort: {_id: 1}
         }]).toArray();
 
         res.status(200).json(document);
